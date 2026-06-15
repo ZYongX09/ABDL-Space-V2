bc93fd68dc37facfec09702cc8a832c537085761
ZYongX09 <3806526113@qq.com>
2026-06-16 03:01:46 +0800

    fix: correct import path in MastodonProfile.jsx

--- client/src/pages/MastodonProfile.jsx
+++ client/src/pages/MastodonProfile.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { apiFetch } from './api';
+import { apiFetch } from '../api';
 
 /**
  * Mastodon-compatible /@:username route handler
